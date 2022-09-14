aee0176a36778cfc7bf25308d89ea314a9a91655
Egor Yakovlev <test@MBP-test.Dlink>
2022-09-14 22:23:15 +0300

    add folders

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
-const app = {
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
+async function start() {
+  const PORT = process.env.PORT || 4000;
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(PORT, () => {
+    console.log("Hello, Egor!", `Server started on port = ${PORT}`);
+  })
 }
+
+start();
